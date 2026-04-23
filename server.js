082c42f2451add2a84978ff812dabc21c5b3bed9
timknutsen <timknutsen@gmail.com>
2026-04-23 11:51:29 +0200

    Use PORT env var for Railway

--- server.js
+++ server.js
@@ -92,5 +92,5 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Chat app running at http://localhost:${PORT}`));
